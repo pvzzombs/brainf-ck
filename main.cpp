95a9fe78941df0b26aff628219a9cb59dbff9419
Miles MJ Jamon <31880601+pvzzombs@users.noreply.github.com>
2019-08-06 01:43:24 -0700

    updated 0.1.3

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int memory[5000];
 int pnt = 0;
 int cnt = 0;
 int c;
-char temp;
+string temp;
 int open = 0;
 int close = 0;
 int prg;
@@ -20,25 +20,50 @@ string fileName;
 string program;
 string str;
 
-int main()
-{
-    system("color 0b");
-    cout << "$ Enter File Path: ";
-    getline(cin, fileName);
-    inFile.open(fileName);
-    if(!inFile){
-        cout << "Error : Cannot Open File !" << endl;
-        system("pause");
-        return 0;
+void collectInput(){
+    cout << "\n$ Input : ";
+    getline(cin,temp);
+    if(temp[0] == '\n' || (temp[0] == '\r' && temp[1] == '\n')){
+        memory[pnt] = 0;
+    }else{
+        memory[pnt] = int(temp[0]);
     }
-    while(getline(inFile, str)){
-        program += str;
+}
+
+void leftBracket(){
+    if(memory[pnt] == 0){
+    open = 0;
+    cnt++;
+        while(cnt < prg){
+            if(program[cnt] == ']' && open == 0){
+                break;
+            }else if(program[cnt] == '['){
+                open++;
+            }else if(program[cnt] == ']'){
+                open--;
+            }
+            cnt++;
+        }
     }
-    //cout << "$ ";
-    //getline(cin, program);
-    prg = program.length();
-    //cout << program << endl;
-    //cout << prg << endl;
+}
+
+void rightBracket(){
+    if(memory[pnt] > 0){
+        close = 0;
+        cnt--;
+        while(cnt >= 0){
+            if(program[cnt] == '[' && close == 0){
+                break;
+            }else if(program[cnt] == ']'){
+                close++;
+            }else if(program[cnt] == '['){
+                close--;
+            }
+            cnt--;
+        }
+    }
+}
+void compile(){
     while(cnt < prg){
         if(Max > 4000000){
             cout << "Error : Maximum iteration exceeded!!!" << endl;
@@ -69,43 +94,39 @@ int main()
         }else if(program[cnt] == '.'){
             cout << char(memory[pnt]);
         }else if(program[cnt] == ','){
-            cout << "\n$ Input : ";
-            cin >> temp;
-            memory[pnt] = int(temp);
+            collectInput();
         }else if(program[cnt] == '['){
-            if(memory[pnt] == 0){
-            open = 0;
-            cnt++;
-            while(cnt < prg){
-                    if(program[cnt] == ']' && open == 0){
-                        break;
-                    }else if(program[cnt] == '['){
-                        open++;
-                    }else if(program[cnt] == ']'){
-                        open--;
-                    }
-                    cnt++;
-                }
-            }
+            leftBracket();
         }else if(program[cnt] == ']'){
-            if(memory[pnt] > 0){
-                close = 0;
-                cnt--;
-                while(cnt >= 0){
-                    if(program[cnt] == '[' && close == 0){
-                        break;
-                    }else if(program[cnt] == ']'){
-                        close++;
-                    }else if(program[cnt] == '['){
-                        close--;
-                    }
-                    cnt--;
-                }
-            }
+            rightBracket();
         }
         Max++;
         cnt++;
     }
+}
+
+int main()
+{
+    system("color 0b");
+    cout << "$ Enter File Path: ";
+    getline(cin, fileName);
+    inFile.open(fileName);
+    if(!inFile){
+        cout << "Error : Cannot Open File !" << endl;
+        system("pause");
+        return 0;
+    }
+    while(getline(inFile, str)){
+        program += str;
+    }
+    //cout << "$ ";
+    //getline(cin, program);
+    prg = program.length();
+    //cout << program << endl;
+    //cout << prg << endl;
+
+    compile();
+
     system("pause");
     return 0;
 }
